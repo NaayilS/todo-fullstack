af5150998ec339974e52a471bac8cbffc705cea9
NaayilS <sadiqnaayil@gmail.com>
2024-10-03 11:33:52 -0500

    fixed frontend app jsx to display properly

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,16 +4,19 @@ import './App.css'
 function App() {
   
   useEffect(() => {
-    async function test(){
+    async function test() {
     const response = await fetch('http://localhost:8080/test')
     const data = await response.json()
+    console.log(data)
   }
    test()
 }, [])
 
   return (
     <>
-       Hello (from Client) !
+       Hello (from Client)!
     </>
   )
 }
+
+export default App
